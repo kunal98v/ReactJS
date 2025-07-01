d019e5a8e93fa78263d3f5e5e596c4f1965e419e
kunal98v <kunal98v@gmail.com>
2025-07-01 21:36:30 +0530

    added useParams in About route

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,22 @@
 import { Header } from "./components/Header";
 import Body from "./components/Body";
-import {Contact} from "./components/Contact";
-import {About} from "./components/About";
-import {Error} from "./components/Error";
-import { BrowserRouter, Routes, Route} from "react-router-dom";
+import { Contact } from "./components/Contact";
+import { About } from "./components/About";
+import { Error } from "./components/Error";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
-
   return (
     <>
-    <BrowserRouter>
-      <Header/>
-      <Routes>
-        <Route path="/" element={<Body />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/contact" element={<Contact />} />
-        <Route path="*" element={<Error />} />
-      </Routes>
-    </BrowserRouter>
-
+      <BrowserRouter>
+        <Header />
+        <Routes>
+          <Route path="/" element={<Body />} />
+          <Route path="/about/:name" element={<About />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="*" element={<Error />} />
+        </Routes>
+      </BrowserRouter>
     </>
   );
 }
--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,6 +1,11 @@
 import React from 'react'
+import { useParams } from 'react-router-dom';
+
 
 export const About = () => {
+
+    const {name} = useParams();
+    console.log(name)
   return (
     <div>About</div>
   )
