e97fa6554b6aedd5c8d8a26ad17c8e97538c4f52
kunal98v <kunal98v@gmail.com>
2025-07-12 23:28:39 +0530

    Made custom hook for online status

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,16 @@
 import React from 'react'
 import { Link } from "react-router-dom";
+import { useOnlineStatus } from '../../useOnlineStatus';
 
 export const Header = () => {
+
+  const onlineStatus = useOnlineStatus()
+
   return (
     <div className='flex bg-pink-200 justify-between shadow-xl w-full'>
         <img className='w-20' src='https://img.pikbest.com/png-images/20241111/-22creative-food-logo-collection-for-culinary-brands-22_11079861.png!w700wp' alt='asd'></img>
         <ul className='flex items-center'>
+            <li className='px-5'>Online Status {onlineStatus ? '🟢' : '🔴'}</li>
             <Link to = '/'><li className='px-5'>Home</li></Link>
             <Link to = '/about'><li className='px-5'>About</li></Link>
             <Link to = '/contact'><li className='px-5'>Contact</li></Link>
---
+++ useOnlineStatus.jsx
@@ -0,0 +1,17 @@
+import React, { useEffect, useState } from "react";
+
+export const useOnlineStatus = () => {
+  const [status, setStatus] = useState(true);
+
+  useEffect(() => {
+    window.addEventListener("online", () => {
+      setStatus(true);
+    });
+
+    window.addEventListener("offline", () => {
+      setStatus(false);
+    });
+  }, []);
+
+  return status;
+};
